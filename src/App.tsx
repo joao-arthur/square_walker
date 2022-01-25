0957e7466d6356a5cf5789827aaa6c1b6b980192
Joao-Arthur <joao.lothamer@gmail.com>
2022-01-25 12:50:20 -0300

    setup style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { ScriptMode } from 'react';
+import { StrictMode, useEffect, useState } from 'react';
 
 export function App() {
+    const [height, setHeight] = useState(window.innerHeight);
+
+    function onHeightChange() {
+        setHeight(window.innerHeight);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', onHeightChange);
+        return () => window.removeEventListener('resize', onHeightChange);
+    }, []);
+
     return (
         <StrictMode>
-            <div className='App'>
+            <div
+                style={{
+                    backgroundColor: 'black',
+                    color: 'white',
+                    width: '100vw',
+                    height
+                }}
+            >
                 That is not dead which can eternal lie, <br /> And with strange
                 aeons even death may die
             </div>
