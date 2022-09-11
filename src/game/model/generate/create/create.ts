42dc568a5e94a103d55c15dce4a95ebb07a7348d
joao-arthur <joao.lothamer@gmail.com>
2022-09-11 14:19:06 -0300

    chore: tsconfig

--- src/game/model/generate/create/create.ts
+++ src/game/model/generate/create/create.ts
@@ -1,5 +1,5 @@
 import { model } from '../../model';
 
-export function create(_: model) {
-    return Array(100).fill(Array(100).fill());
+export function create(_: model): model {
+    return Array(100).fill(Array(100).fill('EMPTY'));
 }
