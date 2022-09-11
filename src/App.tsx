2943efa5738576006408abbf859505e50c0c24d9
joao-arthur <joao.lothamer@gmail.com>
2022-09-11 14:23:13 -0300

    refactor: delete old view

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import { StrictMode, useEffect, useRef } from 'react';
-import { useDidMount, useWillUnmount } from 'rooks';
-import {
-    destroyCanvasPaint,
-    initCanvasPaint,
-    updateCanvasDimensions,
-} from './canvas';
-import { useWindowDimensions } from './useWindowDimensions';
+import { StrictMode } from 'react';
 
 export function App() {
-    const dimensions = useWindowDimensions();
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-
-    useEffect(() => {
-        updateCanvasDimensions(dimensions);
-    }, [dimensions]);
-
-    useDidMount(() => {
-        if (!canvasRef.current)
-            return;
-        const context = canvasRef.current.getContext('2d');
-        if (!context)
-            return;
-        initCanvasPaint(context, dimensions);
-    });
-
-    useWillUnmount(() => {
-        destroyCanvasPaint();
-    });
-
     return (
         <StrictMode>
-            <canvas
-                width={dimensions.width}
-                height={dimensions.height}
-                style={{
-                    width: dimensions.width,
-                    height: dimensions.height,
-                }}
-                ref={canvasRef}
-            />
+            <canvas />
         </StrictMode>
     );
 }
