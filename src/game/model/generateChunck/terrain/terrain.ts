2767290130ce4246a3160593635172ca4456ba5f
joao-arthur <joao.lothamer@gmail.com>
2022-09-13 10:50:58 -0300

    feat: setup MVC

--- src/game/model/generateChunck/terrain/terrain.ts
+++ src/game/model/generateChunck/terrain/terrain.ts
@@ -1,18 +1,14 @@
-import { produce } from 'immer';
+import { mapGroundLevelToModel } from '../../mapGroundLevelToModel/mapGroundLevelToModel';
 import { modelType } from '../../model';
 import { groundLevel } from './groundLevel/groundLevel';
 
 export function terrain(model: modelType) {
-    return produce(model, draft => {
-        const modelGroundLevel = groundLevel({
-            numberOfColumns: 100,
-            minHeight: 20,
-            maxHeight: 80,
-            initialHeight: 50,
-        });
-
-        for (let i = 0; i < 100; i++)
-            for (let j = modelGroundLevel[i] - 1; j < 100; j++)
-                draft[j][i] = 'SOLID_STONE';
+    const modelGroundLevel = groundLevel({
+        numberOfColumns: 100,
+        minHeight: 20,
+        maxHeight: 80,
+        initialHeight: 50,
     });
+
+    return mapGroundLevelToModel(model, modelGroundLevel);
 }
